8dd8be8094c2b1db6bd046b93d69f439f330a2ad
Mayra Medina <111921764+Maynh@users.noreply.github.com>
2023-11-09 22:41:58 -0300

    Subiendo parte de ventas-autos usados

---
+++ proyectoCompleto/js/autosusados.js
@@ -0,0 +1,39 @@
+const productos = [
+{
+    id: 6,
+    productName: 'Ford Escape-Modelo 2013',
+    price: 10500000,
+    quanty : 1,
+    img: '../client/Autos usados/escape-2013-2016-suv-06.jpg',
+
+},
+{
+    id: 7,
+    productName: 'Ford Explorer-Modelo 2016',
+    price: 12300000,
+    quanty : 1,
+    img: '../client/Autos usados/explorer-2016-2019-suv-07.jpg',
+
+},
+{
+    id: 8,
+    productName: 'Ford Figo-Modelo 2016',
+    price: 11790000,
+    quanty : 1,
+    img: '../client/Autos usados/figo-2016-2022-sedan-05.jpg',
+},
+{
+    id: 9,
+    productName: 'Ford Fusion-Modelo 2013',
+    price: 9800000,
+    quanty : 1,
+    img: '../client/Autos usados/fusion-2013-2020-sedan-08.jpg',
+},
+{
+    id: 10,
+    productName: 'Ford Vento-Modelo 2014',
+    price: 9800000,
+    quanty : 1,
+    img: '../client/Autos usados/vento-2014-2019-sedan-02.jpg',
+
+}];
