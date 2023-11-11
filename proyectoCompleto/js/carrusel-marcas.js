0a8ae92467d4486e4170a071041b891eb6a610ce
Bergerat Santiago <70027319+Yago030@users.noreply.github.com>
2023-11-11 00:42:31 -0300

    subiendo arreglos menores ahora de marcas

--- proyectoCompleto/js/carrusel-marcas.js
+++
@@ -1,5 +0,0 @@
-function verMas(enlace) {
-    // Puedes redirigir a una página específica o realizar otras acciones
-    alert('Ver más enlace: ' + enlace);
-    // También puedes usar window.location.href = enlace; para redirigir a una página.
-}
